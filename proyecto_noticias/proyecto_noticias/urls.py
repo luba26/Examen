7795bab458dca818066c5ca8318112657c6bb879
luciano bahamondes <luciano.bnavarro@gmail.com>
2021-07-25 23:56:41 -0400

    inicio

--- proyecto_noticias/proyecto_noticias/urls.py
+++ proyecto_noticias/proyecto_noticias/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app_proyecto/', include('app_proyecto.urls')),
+    path('', include('app_proyecto.urls')),
     
 ]
 
